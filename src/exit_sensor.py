4e67271126e2cf73c3395315fea302dd3eeb48a7
jarryd <jarrydrocks@gmail.com>
2025-11-30 09:54:55 +0800

    feat/implemented exit sensor's update_car_park

--- src/exit_sensor.py
+++ src/exit_sensor.py
@@ -3,4 +3,5 @@ from sensor import Sensor
 
 class ExitSensor(Sensor):
     def update_car_park(self, plate: str) -> None:
-        self.car_park.remove_car(plate)
+        self.car_park.add_car(plate)
+        print(f"Outgoing 🚗 vehicle detected. Plate: {plate}")  # CarPark should be responsible for this
