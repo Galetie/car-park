1006fca80ff6f8dcbfd32598bcfd7560925788a0
jarryd <jarrydrocks@gmail.com>
2025-11-30 08:44:42 +0800

    feat/Added init and str methods for Display

--- src/display.py
+++ src/display.py
@@ -1,2 +1,13 @@
 class Display:
-    pass
+    def __init__(
+            self,
+            id: str = None,
+            message: str = "",
+            is_on: bool = False
+    ):
+        self.id = id
+        self.message = message
+        self.is_on = is_on
+
+    def __str__(self):
+        return f"Display {self.id}: Welcome to the car park."
