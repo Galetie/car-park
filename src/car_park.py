0668bccdcf06c02b3214372157face3b05a3a2ce
jarryd <jarrydrocks@gmail.com>
2025-11-30 08:37:15 +0800

    fix/displays are a list of Display

--- src/car_park.py
+++ src/car_park.py
@@ -2,7 +2,7 @@ from display import Display
 
 
 class CarPark:
-    def __init__(self, location: str, capacity: int, plates: list[str], displays: Display):
+    def __init__(self, location: str, capacity: int, plates: list[str], displays: list[Display]):
         self.location = location
         self.capacity = capacity
         self.plates = plates
