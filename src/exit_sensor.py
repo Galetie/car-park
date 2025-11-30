0ed9c27d2e284d2083856c8b0eb494142d0910b9
jarryd <jarrydrocks@gmail.com>
2025-11-30 09:57:47 +0800

    fix/added override for scan plate method i nexit sensor

--- src/exit_sensor.py
+++ src/exit_sensor.py
@@ -1,7 +1,12 @@
+import random
+
 from sensor import Sensor
 
 
 class ExitSensor(Sensor):
+    def _scan_plate(self):
+        return random.choice(self.car_park.plates)
+
     def update_car_park(self, plate: str) -> None:
         self.car_park.add_car(plate)
         print(f"Outgoing 🚗 vehicle detected. Plate: {plate}")  # CarPark should be responsible for this
