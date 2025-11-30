0035c909210b63705ae2d81b55a83cc815ffb9c6
jarryd <jarrydrocks@gmail.com>
2025-11-30 08:36:16 +0800

    feat/Added CarPark init

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,9 @@
+from display import Display
+
+
 class CarPark:
-    pass
+    def __init__(self, location: str, capacity: int, plates: list[str], displays: Display):
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates
+        self.displays = displays
