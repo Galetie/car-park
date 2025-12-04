cfb89d57cf4de14024598eb1c309526c380e2cd4
jarryd <jarrydrocks@gmail.com>
2025-12-05 07:43:23 +0800

    docs(sensors): add docstring to ExitSensor class

--- src/exit_sensor.py
+++ src/exit_sensor.py
@@ -4,6 +4,13 @@ from sensor import Sensor
 
 
 class ExitSensor(Sensor):
+    """
+    A sensor that detects vehicles exiting the car park.
+    
+    This sensor monitors exit points and removes vehicles from the
+    car park system by selecting from currently registered plates.
+    """
+    
     def _scan_plate(self):
         # Check if the car park has plates to pull from, as this is a hack
         if len(self.car_park.plates) == 0:
