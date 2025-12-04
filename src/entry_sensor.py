f63507db4616eeddfd32494e5345e6f1d33016b2
jarryd <jarrydrocks@gmail.com>
2025-12-05 07:40:21 +0800

    docs(sensors): add docstring to EntrySensor class

--- src/entry_sensor.py
+++ src/entry_sensor.py
@@ -2,6 +2,13 @@ from sensor import Sensor
 
 
 class EntrySensor(Sensor):
+    """
+    A sensor that detects vehicles entering the car park.
+    
+    This sensor monitors entry points and registers new vehicles
+    by adding their license plates to the car park system.
+    """
+    
     def update_car_park(self, plate: str) -> None:
         self.car_park.add_car(plate)
 
